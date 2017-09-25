e6d4fded611a56183939a07f5cc55b0f5b51f597
Gu Hyun Jung <ghj1452@tlab-01.ece.northwestern.edu>
2017-09-24 22:39:54 -0500

    sunday night

--- xv6/kernel/defs.h
+++ xv6/kernel/defs.h
@@ -109,6 +109,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+void  		ps(void);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
